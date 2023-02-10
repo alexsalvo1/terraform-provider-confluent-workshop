a29b46715046b4c31368254db1ba86096ca6a174
alexsalvo1 <alexsalvo1@gmail.com>
2023-02-10 15:52:32 +0100

    change regions

--- dedicated-kafka-acls-azure/main.tf
+++ dedicated-kafka-acls-azure/main.tf
@@ -13,7 +13,7 @@ provider "confluent" {
 }
 
 resource "confluent_environment" "env" {
-  display_name = "Source"
+  display_name = "SAlessandro-Source"
 }
 
 # Stream Governance and Kafka clusters can be in different regions as well as different cloud providers,
@@ -57,7 +57,7 @@ resource "confluent_kafka_cluster" "dedicated" {
 // 'app-manager' service account is required in this configuration to create 'orders' topic and grant ACLs
 // to 'app-producer' and 'app-consumer' service accounts.
 resource "confluent_service_account" "app-manager" {
-  display_name = "app-manager-azure-dedicated"
+  display_name = "app-manager-salessandro-azure-dedicated"
   description  = "Service account to manage 'inventory' Kafka cluster"
 }
 
@@ -68,7 +68,7 @@ resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
 }
 
 resource "confluent_api_key" "app-manager-kafka-api-key" {
-  display_name = "app-manager-kafka-api-key-azure-dedicated"
+  display_name = "app-manager-kafka-api-key-salessandro-azure-dedicated"
   description  = "Kafka API Key that is owned by 'app-manager' service account"
   owner {
     id          = confluent_service_account.app-manager.id
@@ -111,12 +111,12 @@ resource "confluent_kafka_topic" "orders" {
 }
 
 resource "confluent_service_account" "app-consumer" {
-  display_name = "app-consumer-azure-dedicated"
+  display_name = "app-consumer-salessandro-azure-dedicated"
   description  = "Service account to consume from 'orders' topic of 'inventory' Kafka cluster"
 }
 
 resource "confluent_api_key" "app-consumer-kafka-api-key" {
-  display_name = "app-consumer-kafka-api-key-azure-dedicated"
+  display_name = "app-consumer-kafka-api-key-salessandro-azure-dedicated"
   description  = "Kafka API Key that is owned by 'app-consumer' service account"
   owner {
     id          = confluent_service_account.app-consumer.id
@@ -154,12 +154,12 @@ resource "confluent_kafka_acl" "app-producer-write-on-topic" {
 }
 
 resource "confluent_service_account" "app-producer" {
-  display_name = "app-producer-azure-dedicated"
+  display_name = "app-producer-salessandro-azure-dedicated"
   description  = "Service account to produce to 'orders' topic of 'inventory' Kafka cluster"
 }
 
 resource "confluent_api_key" "app-producer-kafka-api-key" {
-  display_name = "app-producer-kafka-api-key-azure-dedicated"
+  display_name = "app-producer-kafka-api-key-salessandro-azure-dedicated"
   description  = "Kafka API Key that is owned by 'app-producer' service account"
   owner {
     id          = confluent_service_account.app-producer.id
